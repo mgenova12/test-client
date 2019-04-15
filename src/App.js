a373a506c4fa4184b060cac1a8bfe1d9c290973e
Marco <mgenova03@gmail.com>
2019-04-15 11:58:15 -0400

    adding test api fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import fetch from 'isomorphic-fetch'
+import runtimeEnv from '@mars/heroku-js-runtime-env'
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      data: ""
+    }
+  }
+
+  componentDidMount() {
+    const url = runtimeEnv().REACT_APP_API_URL
+    fetch(url)
+      .then( res => res.json() )
+      .then( json => this.setState({ data: json }) )
+  }
+
   render() {
+    console.log(this.state.data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn 
-          </a>
-        </header>
-      </div>
+      <p>Data recieved from API: { this.state.data }</p>
     );
   }
 }
